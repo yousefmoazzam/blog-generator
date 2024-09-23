009c04a5f4cf43f1060a81b8770f42e909ccd994
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-09-23 23:41:18 +0100

    Add type signatures to functions

--- hello.hs
+++ hello.hs
@@ -2,12 +2,17 @@ main = putStrLn myhtml
 
 myhtml = makeHtml "My page title" "My page content"
 
+makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
+html_ :: String -> String
 html_ content = "<html>" <> content <> "</html>"
 
+body_ :: String -> String
 body_ content = "<body>" <> content <> "</body>"
 
+head_ :: String -> String
 head_ content = "<head>" <> content <> "</head>"
 
+title_ :: String -> String
 title_ content = "<title>" <> content <> "</title>"
