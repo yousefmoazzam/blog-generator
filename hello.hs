54ea6836278dd77bd542c998060b81eebf8d7f91
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-09-26 00:11:32 +0100

    Add `h1` and `p` tags to outputted string

--- hello.hs
+++ hello.hs
@@ -1,9 +1,9 @@
 main = putStrLn myhtml
 
-myhtml = makeHtml "My page title" "My page content"
+myhtml = makeHtml "My page title" "Some header" "Hello, world!"
 
-makeHtml :: String -> String -> String
-makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+makeHtml :: String -> String -> String -> String
+makeHtml title header paragraph = html_ (head_ (title_ title) <> body_ (h1_ header <> p_ paragraph))
 
 html_ :: String -> String
 html_ = el "html"
