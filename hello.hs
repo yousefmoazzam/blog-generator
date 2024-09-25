020bec4331cce469536f5c5631c238d2eba025c4
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-09-26 00:11:32 +0100

    Add functions for defining `p` and `h1` tags

--- hello.hs
+++ hello.hs
@@ -17,5 +17,11 @@ head_ = el "head"
 title_ :: String -> String
 title_ = el "title"
 
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
 el :: String -> String -> String
 el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
