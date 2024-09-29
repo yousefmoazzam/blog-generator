6f60b8d2df0cbb70734b998680b73a354f726d33
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-09-29 13:56:06 +0100

    Move `render` call out of `myhtml` definition

--- hello.hs
+++ hello.hs
@@ -1,17 +1,16 @@
-main = putStrLn myhtml
+main = putStrLn (render myhtml)
 
+myhtml :: Html
 myhtml =
-  render
-    ( html_
-        "My page title"
-        ( Structure
-            ( el
-                "body"
-                ( getStructureString
-                    ( append_
-                        (h1_ "Some header")
-                        (append_ (p_ "Paragraph 1: Hello, world!") (p_ "Paragraph 2: Some other thing"))
-                    )
+  html_
+    "My page title"
+    ( Structure
+        ( el
+            "body"
+            ( getStructureString
+                ( append_
+                    (h1_ "Some header")
+                    (append_ (p_ "Paragraph 1: Hello, world!") (p_ "Paragraph 2: Some other thing"))
                 )
             )
         )
