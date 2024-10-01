396198e7734e936e285c7ee4b75793853af7c664
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-10-01 21:23:20 +0100

    Use char escape function on user input strings

--- Html.hs
+++ Html.hs
@@ -21,16 +21,16 @@ html_ title bodyStructure =
   Html
     ( el
         "html"
-        ( el "head" (el "title" title)
+        ( el "head" (el "title" (escape title))
             <> el "body" (getStructureString bodyStructure)
         )
     )
 
 p_ :: String -> Structure
-p_ = Structure . el "p"
+p_ = Structure . el "p" . escape
 
 h1_ :: String -> Structure
-h1_ = Structure . el "h1"
+h1_ = Structure . el "h1" . escape
 
 append_ :: Structure -> Structure -> Structure
 append_ (Structure str1) (Structure str2) = Structure (str1 <> str2)
