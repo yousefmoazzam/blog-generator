81e28fca8c72b2ce6426d027a9a8d43148112a5c
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-12-28 14:29:20 +0000

    Add starting-point code for chapter 4
    This is the code that's done for the reader in chapter 4 up until
    section 4.4, where the exercises then ask the reader to build upon the
    code that's already been done for them.

--- Markup.hs
+++ Markup.hs
@@ -4,6 +4,7 @@ module Markup
   )
 where
 
+import Data.Maybe
 import Numeric.Natural
 
 type Document =
@@ -16,3 +17,36 @@ data Structure
   | OrderedList [String]
   | CodeBlock [String]
   deriving (Show)
+
+parse :: String -> Document
+parse = parseLines Nothing . lines
+
+parseLines :: Maybe Structure -> [String] -> Document
+parseLines context txts =
+  case txts of
+    -- done case
+    [] -> maybeToList context
+    -- Heading 1 case
+    ('*' : ' ' : line) : rest ->
+      maybe id (:) context (Heading 1 (trim line) : parseLines Nothing rest)
+    -- Unordered list case
+    ('-' : ' ' : line) : rest ->
+      case context of
+        Just (UnorderedList list) ->
+          parseLines (Just (UnorderedList (list <> [trim line]))) rest
+        _ ->
+          maybe id (:) context (parseLines (Just (UnorderedList [trim line])) rest)
+    -- Paragraph case
+    currentLine : rest ->
+      let line = trim currentLine
+       in if line == ""
+            then
+              maybe id (:) context (parseLines Nothing rest)
+            else case context of
+              Just (Paragraph paragraph) ->
+                parseLines (Just (Paragraph (unwords [paragraph, line]))) rest
+              _ ->
+                maybe id (:) context (parseLines (Just (Paragraph line)) rest)
+
+trim :: String -> String
+trim = unwords . words
