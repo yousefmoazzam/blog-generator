706ff61a44fe5d57fb57cc80a1b8f8b623aa7e6f
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2025-01-01 17:23:36 +0000

    Request confirmation if given output file exists

--- hello.hs
+++ hello.hs
@@ -7,7 +7,15 @@ main =
   getArgs >>= \args ->
     case args of
       -- Open input, process contents, write to output
-      [first, second] -> processInputWriteOutput first second
+      [first, second] ->
+        doesFileExist second >>= \doesExist ->
+          case doesExist of
+            False -> processInputWriteOutput first second
+            True ->
+              confirm >>= \confirmed ->
+                case confirmed of
+                  False -> pure ()
+                  True -> processInputWriteOutput first second
 
 processInputWriteOutput :: FilePath -> FilePath -> IO ()
 processInputWriteOutput input output =
@@ -15,3 +23,15 @@ processInputWriteOutput input output =
     pure
       (Convert.process input contents)
       >>= \processed -> writeFile output processed
+
+confirm :: IO Bool
+confirm =
+  putStrLn "Are you sure? (y/n)"
+    *> getLine
+    >>= \answer ->
+      case answer of
+        "y" -> pure True
+        "n" -> pure False
+        _ ->
+          putStrLn "Invalid response. use y or n"
+            *> confirm
