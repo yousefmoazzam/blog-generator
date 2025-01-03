58c087930b782e69b5008caaccb9a4e2dc339ebc
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2025-01-03 00:30:31 +0000

    Add provided code for CLI `convert` command

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,65 @@
 module Main where
 
 import qualified BlogGenerator
+import Options.Applicative
 
 main :: IO ()
 main = BlogGenerator.main
+
+data Options
+  = ConvertSingle SingleInput SingleOutput
+  | ConvertDir FilePath FilePath
+  deriving (Show)
+
+data SingleInput
+  = Stdin
+  | InputFile FilePath
+  deriving (Show)
+
+data SingleOutput
+  = Stdout
+  | OutputFile FilePath
+  deriving (Show)
+
+inp :: Parser FilePath
+inp =
+  strOption
+    ( long "input"
+        <> short 'i'
+        <> metavar "FILE"
+        <> help "Input file"
+    )
+
+out :: Parser FilePath
+out =
+  strOption
+    ( long "output"
+        <> short 'o'
+        <> metavar "FILE"
+        <> help "Output file"
+    )
+
+pInputFile :: Parser SingleInput
+pInputFile = fmap InputFile parser
+  where
+    parser =
+      strOption
+        ( long "input"
+            <> short 'i'
+            <> metavar "FILE"
+            <> help "Input file"
+        )
+
+pOutputFile :: Parser SingleOutput
+pOutputFile = fmap OutputFile parser
+  where
+    parser =
+      strOption
+        ( long "output"
+            <> short 'o'
+            <> metavar "FILE"
+            <> help "Output file"
+        )
+
+pConvertSingle :: Parser Options
+pConvertSingle = liftA2 ConvertSingle pInputFile pOutputFile
