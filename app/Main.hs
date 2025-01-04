e4db44695319771e09b93c9052e0f84dd49e68a6
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2025-01-04 16:37:29 +0000

    Add skeleton CLI and working `convert` command

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,35 @@
 module Main where
 
 import qualified BlogGenerator
+import OptParse
+import Options.Applicative (execParser)
+import System.IO (IOMode (ReadMode, WriteMode), hClose, openFile, stdin, stdout)
 
 main :: IO ()
-main = BlogGenerator.main
+main =
+  execParser OptParse.opts >>= \args ->
+    case args of
+      OptParse.ConvertDir _ _ ->
+        putStrLn "todo"
+      OptParse.ConvertSingle input output ->
+        ( case input of
+            OptParse.Stdin ->
+              pure System.IO.stdin
+            OptParse.InputFile inPath ->
+              openFile inPath ReadMode
+        )
+          >>= \inHandle ->
+            ( case output of
+                OptParse.Stdout ->
+                  pure System.IO.stdout
+                OptParse.OutputFile outPath ->
+                  openFile outPath WriteMode
+            )
+              >>= \outHandle ->
+                BlogGenerator.convertSingle inFilename inHandle outHandle
+                  *> hClose inHandle
+                  *> hClose outHandle
+        where
+          inFilename = case input of
+            OptParse.Stdin -> "stdin"
+            OptParse.InputFile path -> path
