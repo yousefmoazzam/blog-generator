c2683b197a21a63fc436af95d6038c35b89b460c
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2025-01-09 23:12:22 +0000

    Add implementation of `applyIoOnList`

--- src/BlogGenerator/Directory.hs
+++ src/BlogGenerator/Directory.hs
@@ -2,6 +2,7 @@ module BlogGenerator.Directory (buildIndex) where
 
 import BlogGenerator.Html as Html
 import BlogGenerator.Markup as Markup
+import Control.Exception (SomeException (..), catch, displayException)
 import Data.Maybe (listToMaybe)
 
 buildIndex :: [(FilePath, Markup.Document)] -> Html.Html
@@ -19,3 +20,14 @@ buildIndex input =
       createLinkAndSummary (path, doc) =
         Html.p_ (Html.link_ path (Html.txt_ "")) <> summariseDoc doc
    in Html.html_ title (top <> foldMap createLinkAndSummary input)
+
+applyIoOnList :: (a -> IO b) -> [a] -> IO [(a, Either String b)]
+applyIoOnList func vals =
+  sequenceA (map process vals)
+  where
+    process input =
+      catch
+        ( func input >>= \output ->
+            pure (input, Right output)
+        )
+        (\e -> pure (input, Left $ displayException (e :: SomeException)))
