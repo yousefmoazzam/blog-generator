db0eeb19f22309254dc9ae1771f95faa74f39bd4
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2025-01-11 22:06:30 +0000

    Add provided code for `Env` module

---
+++ src/BlogGenerator/Env.hs
@@ -0,0 +1,11 @@
+module BlogGenerator.Env (Env, defaultEnv) where
+
+data Env
+  = Env
+  { eBlogName :: String,
+    eStylesheetPath :: FilePath
+  }
+  deriving (Show)
+
+defaultEnv :: Env
+defaultEnv = Env "Test Blog" "style.css"
